8a26e03d52a70e2289f842ec7d3205152167d131
copriot <developer.zeki@gmail.com>
2024-12-23 21:10:50 +0300

    Buttons component prepared Leaflet map view configured

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import List from "./pages/List";
 import Map from "./pages/Map";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Buttons from "./components/Buttons";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Header />
+        <Buttons />
         <Routes>
           <Route path="/" element={<Map />} />
           <Route path="/list" element={<List />} />
